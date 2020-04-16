892b95aa6d32174ee5514bb91deb0fe6bc823f72
Carmine Lentisco <carminelentisco@gmail.com>
2020-04-16 16:43:06 +0200

    JS

--- script/main.js
+++ script/main.js
@@ -1,5 +1,41 @@
-jQuery( document ).ready( function($) {
+$( document ).ready( function() {
 
-    console.log('JS');
+    var listItems = ['Pane', 'Pasta', 'Fagioli', 'Pizza'];
+    var list = $('.list');
+
+    for (var i = 0; i < listItems.length; i++ ){
+
+        // Clone del template
+        var elementList = $('.template li').clone();
+
+        // Aggingo testo 
+        elementList.prepend(listItems[i]);
+
+        // Aggingi il clone alla lista
+        list.append(elementList);
+    }
+
+    // Rimozione items
+    $('.list li i').click(function(){
+        $(this).parent().remove();
+    });
     
+    var elementInput = $('.add-element');
+    // Aggingi un elemento tramite l'input 
+    elementInput.keyup(function (e) { 
+
+        if (e.which == 13) {
+            var text = elementInput.val().trim();
+            elementInput.val("");
+            
+            if (text !== '') {
+                var elementNew = $('.template li').clone();
+                elementNew.prepend(text);
+                list.append(elementNew);
+            }
+            
+        }
+    });
+
+
 } ); // <------------------------------ End page
